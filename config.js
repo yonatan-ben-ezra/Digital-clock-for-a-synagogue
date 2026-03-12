8352e85d61ded0f749f1982fe78abb85ac272e56
yonatan-ben-ezra <yonatan513.be@gmail.com>
2026-03-12 17:33:25 +0200

    Add configuration file for clock app

---
+++ config.js
@@ -0,0 +1,9 @@
+// Configuration file for the clock app
+const DEFAULT_CONFIG = {
+    timerMinutes: 15,
+    location: 'זאב פלק 18, ירושלים',
+    latitude: 31.7732,
+    longitude: 35.2208,
+    timeFormat: '24h',
+    language: 'he'
+};
